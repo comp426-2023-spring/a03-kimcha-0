084baee27b981ccd9d22ff66e49ee8439abb98af
David <davidcha@email.unc.edu>
2023-03-06 13:57:37 -0500

    returns object instead of string

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -8,7 +8,9 @@ const args = minimist(process.argv.slice(2));
 
 function rps() {
   const results = ['rock', 'paper', 'scissors'];
-  return results[Math.floor(Math.random() * results.length)];
+  return {
+	   "player": results[Math.floor(Math.random() * results.length)]
+  };
 }
 
 if (args.h) {
