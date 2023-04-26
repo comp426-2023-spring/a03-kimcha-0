8e9dc770fa08038ba83c8547c7ec9d235baff10d
David <davidcha@email.unc.edu>
2023-04-26 14:27:23 -0400

    need to finish configuring JSON object to be returned by rps() function

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -10,19 +10,39 @@ function rps(opponent) {
   const results = ["rock", "paper", "scissors"];
   let player = results[Math.floor(Math.random() * results.length)];
   const opponent = opponent.toLowerCase();
+  let result = "";
+
   if (opponent) {
     if (opponent === "rock") {
-      
+      if (player === "rock") {
+        result = "tie";
+      } else if (player == "scissors") {
+          result = "win";
+      } else {
+          result = "lose";
+      }
     } else if (opponent == "scissors") {
-    
+      if (player === "rock") {
+        result = "lose";
+      } else if (player == "scissors") {
+          result = "tie";
+      } else {
+          result = "win";
+      }
     } else if (opponent == "paper") {
-    
+      if (player === "rock") {
+        result = "win";
+      } else if (player == "scissors") {
+          result = "lose";
+      } else {
+          result = "tie";
+      }
     } else {
-    
+      
     } 
   } else {
     return JSON.stringify({
-      player: results[Math.floor(Math.random() * results.length)],
+      "player": results[Math.floor(Math.random() * results.length)],
     });
   }
 }
@@ -38,7 +58,7 @@ if (args.h) {
 
       Examples:
         node-rps	Return JSON with single player RPS result.
-			e.g. {"player": "rock"}
+		e.g. {"player": "rock"}
         node-rps rock	Return JSON with results for RPS played against a simulated opponent.
 			e.g. {"player":"rock", "opponent": "scissors", "result": "win"}
     `);
